fe8daf02d7f21c0c1198016e9934d99ba203651d
Veerain Grover <veerain@blogvault.net>
2020-07-13 17:21:30 +0530

    Revert "User Authentication complete"
    This reverts commit 3eff5e89a16a872e374909b19d75974c99773bc8.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#index"
-  get "signup", to: "users#new"
-  get "login", to: "sessions#new"
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
-
-  resources :users, except: [:new]
 
   resources :articles
 end
