3eff5e89a16a872e374909b19d75974c99773bc8
Veerain Grover <veerain@blogvault.net>
2020-07-13 10:52:16 +0530

    User Authentication complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_10_114316) do
+ActiveRecord::Schema.define(version: 2020_07_13_050712) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.integer "user_id"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "comment"
+    t.text "body"
+    t.string "username"
+    t.integer "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+  end
+
+  add_foreign_key "comments", "articles"
 end
