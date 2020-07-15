a6924bfd00679423cbc3d3c154101ee2c7da6383
Veerain Grover <veerain@blogvault.net>
2020-07-15 14:08:57 +0530

    minor fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :path => 'u'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "pages#index"
+  root "articles#index"
 
   resources :articles
   resources :users_admin
